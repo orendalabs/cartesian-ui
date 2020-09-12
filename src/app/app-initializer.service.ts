790dcbb6bab60b38163a3c5e03856213d90c9b33
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 09:31:19 +0500

    Updates app initializer as service.

--- src/app/app-initializer.service.ts
+++ src/app/app-initializer.service.ts
@@ -10,7 +10,7 @@ import { environment } from '../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
-export class AppInitializer {
+export class AppInitializerService {
   constructor(
     private _injector: Injector,
     private _platformLocation: PlatformLocation,
