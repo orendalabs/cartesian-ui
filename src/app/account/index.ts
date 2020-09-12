9283efecb577e6689e384fefd1e75485c2610209
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 10:06:50 +0500

    add account module index file to manage module exports.

---
+++ src/app/account/index.ts
@@ -0,0 +1,6 @@
+export { AuthService } from './shared/services/auth.service';
+export { AuthHttpService } from './shared/services/http/auth.http';
+export { AccountLanguagesComponent } from './shared/layout/account-languages.component';
+export { AccountHeaderComponent } from './shared/layout/account-header.component';
+export { AccountFooterComponent } from './shared/layout/account-footer.component';
+export * from './store'
