b52156b4a5c9cb1121a883320dba9c5409409ed1
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 09:51:39 +0500

    Moves auth service to account module.

--- src/app/account/shared/services/auth.service.ts
+++ src/app/account/shared/services/auth.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { finalize } from 'rxjs/operators';
-import { TokenService, LogService, UtilsService } from '@cartesian-ui/ng-axis';
 import { AppConstants } from '@cartesian-ui/ng-axis';
+import { TokenService, LogService, UtilsService } from '@cartesian-ui/ng-axis';
 import { UrlHelper } from '@shared/helpers/url.helper';
-import {
-    AuthenticateModel,
-    AuthenticateResultModel,
-    TokenAuthServiceProxy,
-} from '@shared/service-proxies/service-proxies';
+import { AuthToken, LoginForm } from "@shared/models";
 
 @Injectable()
-export class AppAuthService {
-    authenticateModel: AuthenticateModel;
-    authenticateResult: AuthenticateResultModel;
+export class AuthService {
+    authenticateModel: LoginForm;
+    authenticateResult: AuthToken;
     rememberMe: boolean;
 
     constructor(
-        private _tokenAuthService: TokenAuthServiceProxy,
         private _router: Router,
         private _utilsService: UtilsService,
         private _tokenService: TokenService,
@@ -39,73 +33,60 @@ export class AppAuthService {
         }
     }
 
-    authenticate(finallyCallback?: () => void): void {
-        finallyCallback = finallyCallback || (() => { });
+    public processAuthenticateResult(authenticateResult: AuthToken) {
 
-        this._tokenAuthService
-            .authenticate(this.authenticateModel)
-            .pipe(
-                finalize(() => {
-                    finallyCallback();
-                })
-            )
-            .subscribe((result: AuthenticateResultModel) => {
-                this.processAuthenticateResult(result);
-            });
-    }
-
-    private processAuthenticateResult(
-        authenticateResult: AuthenticateResultModel
-    ) {
         this.authenticateResult = authenticateResult;
 
         if (authenticateResult.accessToken) {
+
+            // Save to local storage
+            authenticateResult.save();
+
             // Successfully logged in
             this.login(
                 authenticateResult.accessToken,
-                authenticateResult.encryptedAccessToken,
-                authenticateResult.expireInSeconds,
+                authenticateResult.refreshToken,
+                authenticateResult.expiresIn,
                 this.rememberMe
             );
         } else {
             // Unexpected result!
-
             this._logService.warn('Unexpected authenticateResult!');
-            this._router.navigate(['account/login']);
+            // this._router.navigate(['account/login']);
         }
     }
 
     private login(
         accessToken: string,
-        encryptedAccessToken: string,
-        expireInSeconds: number,
+        refreshToken: string,
+        expiresIn: number,
         rememberMe?: boolean
     ): void {
         const tokenExpireDate = rememberMe
-            ? new Date(new Date().getTime() + 1000 * expireInSeconds)
+            ? new Date(new Date().getTime() + 1000 * expiresIn)
             : undefined;
 
         this._tokenService.setToken(accessToken, tokenExpireDate);
 
-        this._utilsService.setCookieValue(
-            AppConstants.authorization.encryptedAuthTokenName,
-            encryptedAccessToken,
-            tokenExpireDate,
-            axis.appPath
-        );
+        // this._utilsService.setCookieValue(
+        //     AppConstants.authorization.encryptedAuthTokenName,
+        //     accessToken,
+        //     tokenExpireDate,
+        //     axis.appPath
+        // );
 
-        let initialUrl = UrlHelper.initialUrl;
-        if (initialUrl.indexOf('/login') > 0) {
-            initialUrl = AppConstants.appBaseUrl;
-        }
+        // let initialUrl = UrlHelper.initialUrl;
+        // if (initialUrl.indexOf('/login') > 0) {
+        //     initialUrl = AppConstants.appBaseUrl;
+        // }
 
-        location.href = initialUrl;
+        // location.href = initialUrl;
     }
 
     private clear(): void {
-        this.authenticateModel = new AuthenticateModel();
-        this.authenticateModel.rememberClient = false;
+        this.authenticateModel = new LoginForm();
+        this.authenticateModel.remember = false;
         this.authenticateResult = null;
-        this.rememberMe = false;
+        this.rememberMe         = false;
     }
 }
