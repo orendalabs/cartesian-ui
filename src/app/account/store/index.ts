7a8b8a772ff23a88813d74f9cf9b0a6198072386
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 10:00:33 +0500

    Adds account module feature store, index file to manage exports.

---
+++ src/app/account/store/index.ts
@@ -0,0 +1,13 @@
+import * as actions from './auth.action';
+import { AuthEffects } from './auth.effect';
+import {
+  AuthState,
+  reducer,
+} from './auth.reducer';
+
+export {
+  actions,
+  AuthState,
+  AuthEffects,
+  reducer
+}
