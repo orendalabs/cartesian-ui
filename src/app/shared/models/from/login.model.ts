2cac36dd14f87e1789c061964f9b657881831346
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 09:42:06 +0500

    Updates login form model.

---
+++ src/app/shared/models/from/login.model.ts
@@ -0,0 +1,17 @@
+export interface ILoginForm {
+  email:    string;
+  password: string;
+  remember: boolean;
+}
+
+export class LoginForm {
+  public email:    string;
+  public password: string;
+  public remember: boolean;
+
+  constructor(loginForm?: ILoginForm) {
+    this.email    = loginForm ? loginForm.email : '';
+    this.password = loginForm ? loginForm.password : '';
+    this.remember = loginForm ? loginForm.remember :false;
+  }
+}
