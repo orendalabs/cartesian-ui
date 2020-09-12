01fcb586a1676b6b631a1e0daa4317924b756858
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 10:05:33 +0500

    Updates login container component and form html.

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,39 +1,43 @@
-import { Component, Injector } from '@angular/core';
-import { AxisSessionService } from '@cartesian-ui/ng-axis';
-import { AppComponentBase } from '@shared/app-component-base';
-import { accountModuleAnimation } from '@shared/animations/routerTransition';
-import { AppAuthService } from '@shared/auth/app-auth.service';
+import { Component, ChangeDetectionStrategy, Injector, OnInit } from '@angular/core';
+import { BaseComponent } from '@shared/layout';
+import { accountModuleAnimation } from '@shared/animations';
+import { AccountSandbox } from '../account.sandbox';
+import { LoginForm } from '@shared/models';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './login.component.html',
-  animations: [accountModuleAnimation()]
+  animations: [accountModuleAnimation()],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent extends AppComponentBase {
+export class LoginComponent extends BaseComponent implements OnInit {
   submitting = false;
+  form: LoginForm = null;
 
-  constructor(
-    injector: Injector,
-    public authService: AppAuthService,
-    private _sessionService: AxisSessionService
-  ) {
+  constructor( injector: Injector, private _sandbox: AccountSandbox ) {
     super(injector);
   }
 
+  ngOnInit(){
+    this.form = new LoginForm();
+  }
+
   get multiTenancySideIsTeanant(): boolean {
-    return this._sessionService.tenantId > 0;
+    //return this._sessionService.tenantId > 0;
+    return false;
+    // TODO: Check Is mutlti tenancy available, provide option to select/change/enter tenant Id
   }
 
   get isSelfRegistrationAllowed(): boolean {
-    if (!this._sessionService.tenantId) {
-      return true; // false
-    }
-
+    // if (!this._sessionService.tenantId) {
+    //   return true; // false
+    // }
     return true;
+    //TODO: If self registeration is allowed or not
   }
 
   login(): void {
+    this._sandbox.login(this.form);
     this.submitting = true;
-    this.authService.authenticate(() => (this.submitting = false));
   }
 }
