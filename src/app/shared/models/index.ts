b98e9d4d20234041639d5572cd9da08a4e670a05
Syed Ali Murtaza <ali@kazmi.me>
2020-09-12 09:43:51 +0500

    Adds models/index file to handle model exports.

---
+++ src/app/shared/models/index.ts
@@ -0,0 +1,7 @@
+// UI Form Models
+export * from './from/login.model';
+export * from './from/register.model'
+
+// Domain Models
+export * from './domain/auth-token.model';
+export * from './domain/user.model';
