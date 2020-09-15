4ef2cd6965adcd3e33306c660b138167c82e60bb
Syed Ali Murtaza <ali@kazmi.me>
2020-09-15 22:12:33 +0500

    Adds index file to manage store session state.

---
+++ src/app/shared/store/session/index.ts
@@ -0,0 +1,13 @@
+import * as actions from './session.action';
+import { SessionEffect } from './session.effect';
+import {
+  SessionState,
+  reducer,
+} from './session.reducer';
+
+export {
+  actions,
+  SessionState,
+  SessionEffect,
+  reducer
+}
