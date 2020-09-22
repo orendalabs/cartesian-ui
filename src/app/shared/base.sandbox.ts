125f275982d0d463d089e6ab40bef6b2e99e752a
Syed Ali Murtaza <ali@kazmi.me>
2020-09-22 10:59:28 +0500

    Removes deprecated functions, add session service, to make available to all extended sandbox classes.

--- src/app/shared/base.sandbox.ts
+++ src/app/shared/base.sandbox.ts
@@ -1,38 +1,15 @@
-import { Store, select }    from '@ngrx/store';
+import { Injector } from '@angular/core'
 import { localeDateString } from '@cartesian-ui/ng-axis';
-import { AuthToken, User }  from '@shared/models';
-import {
-  State,
-  getLoggedUser,
-  getAuthenticated,
-  getSelectedCulture
-}                           from '@app/app.store';
-import { actions as authActions }  from '@app/account';
+import {SessionService} from "@shared/services";
 
 
 export abstract class Sandbox {
 
-  public loggedUser$      = this.store.pipe(select(getLoggedUser));
-  public isAuthenticated$ = this.store.pipe(select(getAuthenticated));
-  public culture$         = this.store.pipe(select(getSelectedCulture));
-  public culture:     string;
+  protected _sessionService: SessionService;
+  public culture: string;
 
-  constructor(protected store: Store<State>) {}
-
-  /**
-   * Pulls user from local storage and saves it to the store
-   */
-  public loadLoggedUser(): void {
-    var user = JSON.parse(localStorage.getItem('loggedUser'));
-    this.store.dispatch(authActions.addUserAction({ user: new User(user)}));
-  }
-
-  /**
-   * Pulls token from local storage and saves it to the store
-   */
-  public loadAuthToken(): void {
-    var token = JSON.parse(localStorage.getItem('authToken'));
-    this.store.dispatch(authActions.addAuthTokenAction( { authToken: new AuthToken(token)}));
+  constructor(protected injector: Injector) {
+    this._sessionService = injector.get(SessionService);
   }
 
   /**
