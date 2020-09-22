ca488c5ad6ca606dfdc6090efe202edacd9ed43a
Syed Ali Murtaza <ali@kazmi.me>
2020-09-22 11:21:59 +0500

    Adds index files to manage exports.

---
+++ src/app/account/models/index.ts
@@ -0,0 +1,9 @@
+// UI Form Models
+export * from './form/login.model';
+export * from './form/register.model'
+export * from './form/tenant.model'
+
+// Domain Models
+export * from './domain/auth-token.model';
+export * from './domain/tenant.model';
+export * from './domain/user.model';
---
+++ src/app/account/shared/ui/index.ts
@@ -0,0 +1,3 @@
+export { AccountLanguagesComponent } from './account-languages.component';
+export { AccountHeaderComponent } from './account-header.component';
+export { AccountFooterComponent } from './account-footer.component';
