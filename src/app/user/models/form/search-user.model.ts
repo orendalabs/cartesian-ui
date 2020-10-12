ce30dd3758775cdf30aaacc6e4c5fa7feee730d1
Syed Ali Murtaza <ali@kazmi.me>
2020-10-12 12:36:15 +0500

    Updates user search form model

--- src/app/user/models/form/search-user.model.ts
+++ src/app/user/models/form/search-user.model.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { WhereItem } from '@cartesian-ui/ng-axis';
 
+@Injectable()
 export class SearchUserForm {
   name: WhereItem = { column: 'name', operator: '=', value: null };
   email: WhereItem = { column: 'email', operator: '=', value: null };
