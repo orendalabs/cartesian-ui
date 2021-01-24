4544b242369e11b2a4856d9723633e382667b3bd
Osama Khan <Osama6756@gmail.com>
2021-01-24 15:17:49 +0500

    Added SettingState to App State

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,7 +1,9 @@
 import { AccountState } from '@app/account/store';
 import { UserState } from '@app/user/store';
+import { SettingState } from './settings/store/setting.state';
 
 export interface State {
   account: AccountState;
   user: UserState;
+  setting: SettingState;
 }
