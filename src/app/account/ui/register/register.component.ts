d293771715e529287a74dc592909676c0d5c078b
Osama Khan <Osama6756@gmail.com>
2021-01-24 14:48:12 +0500

    Made login and register forms reactive.
    Both forms now use FormGroup for form inputs and validation.

--- src/app/account/ui/register/register.component.ts
+++ src/app/account/ui/register/register.component.ts
@@ -10,46 +10,79 @@ import {
 import { accountModuleAnimation } from '@app/core/animations';
 import { AuthService } from '../../shared';
 
+import { AccountSandbox } from '../../account.sandbox';
+import { RegisterForm } from '../../models/form/register.model'
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormHelper } from '../helpers/form.helper';
+
 @Component({
   templateUrl: './register.component.html',
   animations: [accountModuleAnimation()],
 })
 export class RegisterComponent extends BaseComponent {
-  model: RegisterInput = new RegisterInput();
+  formGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, FormHelper.emailValidator()]),
+    name: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    gender: new FormControl('Gender...', [Validators.required, FormHelper.genderValidator()]),
+    dob: new FormControl('', [Validators.required, FormHelper.dateFormatValidator(), FormHelper.dobValidator(12)])
+  }
+  )
   saving = false;
 
   constructor(
     injector: Injector,
     private _accountService: AccountServiceProxy,
     private _router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    public _sandbox: AccountSandbox
   ) {
     super(injector);
   }
 
   save(): void {
-    this.saving = true;
-    this._accountService
-      .register(this.model)
-      .pipe(
-        finalize(() => {
-          this.saving = false;
-        })
-      )
-      .subscribe((result: RegisterOutput) => {
-        if (!result.canLogin) {
-          this.notify.success(this.l('SuccessfullyRegistered'));
-          this._router.navigate(['/login']);
-          return;
-        }
 
-        // Autheticate
-        // this.saving = true;
-        // this.authService.authenticateModel.userNameOrEmailAddress = this.model.userName;
-        // this.authService.authenticateModel.password = this.model.password;
-        // this.authService.authenticate(() => {
-        //   this.saving = false;
-        // });
-      });
+    if (this.formGroup.valid) {
+      this.saving = true;
+      let form = new RegisterForm();
+      form.email = this.formGroup.controls['email'].value;
+      form.name = this.formGroup.controls['name'].value;
+      form.password = this.formGroup.controls['password'].value;
+      form.gender = this.formGroup.controls['gender'].value;
+      form.birth = this.formGroup.controls['dob'].value;
+      this._sandbox.register(form);
+    }
+
+    //this.saving = true;
+    //this._accountService
+    //  .register(this.model)
+    //  .pipe(
+    //    finalize(() => {
+    //      this.saving = false;
+    //    })
+    //  )
+    //  .subscribe((result: RegisterOutput) => {
+    //    if (!result.canLogin) {
+    //      this.notify.success(this.l('SuccessfullyRegistered'));
+    //      this._router.navigate(['/login']);
+    //      return;
+    //    }
+
+    // Autheticate
+    // this.saving = true;
+    // this.authService.authenticateModel.userNameOrEmailAddress = this.model.userName;
+    // this.authService.authenticateModel.password = this.model.password;
+    // this.authService.authenticate(() => {
+    //   this.saving = false;
+    // });
+    //});
+  }
+
+  getFormClasses = (e: AbstractControl): string => {
+    return FormHelper.getFormClasses(e);
+  }
+
+  getError = (e: any): string => {
+    return FormHelper.getErrorMessage(e);
   }
 }
