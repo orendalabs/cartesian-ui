cc16ca7ffba68755444685d39279b547675f9106
Osama Khan <Osama6756@gmail.com>
2021-01-29 21:26:40 +0500

    Added PermissionListComponent
    This component lists all the available permissions using ngx-datatable

---
+++ src/app/authorization/ui/permission-list/permission-list.component.ts
@@ -0,0 +1,67 @@
+import { AfterViewInit, Component, ElementRef, Injector, OnInit, ViewChild } from '@angular/core';
+import { AuthorizationSandbox } from '@app/authorization/authorization.sandbox';
+import { SearchPermissionForm } from '@app/authorization/models/form/search-permission.model';
+import { Permission } from '@app/authorization/models/permission.model';
+import { ListingControlsComponent } from '@app/core/ui';
+import { RequestCriteria } from '@cartesian-ui/ng-axis';
+
+@Component({
+  selector: 'permission-list',
+  templateUrl: './permission-list.component.html'
+})
+
+export class PermissionListComponent extends ListingControlsComponent<Permission, SearchPermissionForm>
+implements OnInit, AfterViewInit {
+@ViewChild('dtContainer') dtContainer: ElementRef;
+
+constructor(
+  protected _sandbox: AuthorizationSandbox,
+  injector: Injector
+) {
+  super(injector);
+}
+
+registerEvents = () => {
+  this.subscriptions.push(
+    this._sandbox.permissionsFetchData$.subscribe(
+      (data: Permission[]) => {
+        this.data = data;
+        this.ui.clearBusy();
+        this.isTableLoading = false;
+      })
+  )
+  this.subscriptions.push(
+    this._sandbox.permissionsFetchMeta$.subscribe(
+      (meta: any) => {
+        if (meta) {
+          this.pagination = meta ? meta.pagination : null;
+        }
+      })
+  )
+}
+
+unregisterEvents = () => {
+  this.subscriptions.forEach((sub) => sub.unsubscribe())
+}
+
+ngOnInit(): void {
+  this.criteria = new RequestCriteria<SearchPermissionForm>(
+    new SearchPermissionForm()
+  ).limit(2);
+  this.registerEvents();
+}
+
+ngAfterViewInit(): void {
+  this.reloadTable();
+}
+
+protected list(): void {
+  this.ui.setBusy(this.dtContainer.nativeElement);
+  this.isTableLoading = true;
+  this._sandbox.fetchPermissions(this.criteria);
+}
+
+protected delete() { }
+
+
+}
