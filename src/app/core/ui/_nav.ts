3d0db0c1184f6a65fd7ef300c8ea5a58baeabefb
Osama Khan <Osama6756@gmail.com>
2021-01-29 21:28:15 +0500

    Added Authorization Module links in navigation.

--- src/app/core/ui/_nav.ts
+++ src/app/core/ui/_nav.ts
@@ -32,6 +32,52 @@ export const navItems: INavData[] = [
     ],
   },
   {
+    name: 'Authorization',
+    url: '/authorization',
+    icon: 'icon-lock',
+    children: [
+      {
+        name: 'Roles',
+        url: '/authorization/roles',
+        icon: 'icon-people',
+        children: [
+          {
+            name: 'List',
+            url: '/authorization/roles/list',
+            icon: 'icon-list',
+          },
+          {
+            name: 'Create',
+            url: '/authorization/roles/create',
+            icon: 'icon-plus',
+          },
+          {
+            name: 'Manage',
+            url: '/authorization/roles/manage',
+            icon: 'icon-wrench',
+          },
+        ]
+      },
+      {
+        name: 'Permissions',
+        url: '/authorization/permissions',
+        icon: 'icon-check',
+        children: [
+          {
+            name: 'List',
+            url: '/authorization/permissions/list',
+            icon: 'icon-list',
+          },
+          {
+            name: 'Assign',
+            url: '/authorization/permissions/assign',
+            icon: 'icon-link',
+          },
+        ]
+      }
+    ],
+  },
+  {
     divider: true,
   },
 ];
