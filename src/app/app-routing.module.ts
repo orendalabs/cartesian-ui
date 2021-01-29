42e32d62f3a8d0b6b642e443822f967abbfeff92
Osama Khan <Osama6756@gmail.com>
2021-01-29 21:23:32 +0500

    Added routing to authorization module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,6 +42,11 @@ const routes: Routes = [
         loadChildren: () =>
           import('@app/user/user.module').then((m) => m.UserModule),
       },
+      {
+        path: 'authorization',
+        loadChildren: () =>
+          import('@app/authorization/authorization.module').then((m) => m.AuthorizationModule),
+      },
     ],
   },
 ];
