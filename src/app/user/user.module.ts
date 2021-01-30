3f64792c0aee0e15e88ad1c3760d7af2c5cca2a8
Osama Khan <Osama6756@gmail.com>
2021-01-30 20:37:30 +0500

    Added new components in UserModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -10,16 +10,21 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AxisHttpInterceptor } from '@cartesian-ui/ng-axis';
 import { UserComponent } from './user.component';
-import { UserListComponent } from './ui/user-list.component';
+import { UserListComponent } from './ui/user-list/user-list.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserHttpService } from './shared';
 import { UserSandbox } from './user.sandbox';
 import { userFeatureKey, userReducers, UserEffects } from './store';
+import { UserDetailComponent } from './ui/user-detail/user-detail.component';
+import { UserProfileComponent } from './ui/user-profile/user-profile.component';
+import { UserCreateComponent } from './ui/user-create/user-create.component';
+import { UserUpdateComponent } from './ui/user-update/user-update.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     UserRoutingModule,
     BsDropdownModule,
     ButtonsModule.forRoot(),
@@ -27,7 +32,7 @@ import { userFeatureKey, userReducers, UserEffects } from './store';
     StoreModule.forFeature(userFeatureKey, userReducers),
     EffectsModule.forFeature([UserEffects]),
   ],
-  declarations: [UserComponent, UserListComponent],
+  declarations: [UserComponent, UserListComponent, UserDetailComponent, UserProfileComponent, UserCreateComponent, UserUpdateComponent],
   providers: [
     UserHttpService,
     UserSandbox,
