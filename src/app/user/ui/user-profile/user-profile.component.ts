6668c2f801e4f7fba9efd484e726751b7a3fd2e1
Osama Khan <Osama6756@gmail.com>
2021-01-30 20:37:03 +0500

    Added UserProfileComponent
    This component shows the current authenticated user

---
+++ src/app/user/ui/user-profile/user-profile.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSandbox } from '@app/user/user.sandbox';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'user-profile',
+  templateUrl: './user-profile.component.html',
+})
+export class UserProfileComponent implements OnInit {
+
+  token;
+  user;
+
+  subscriptions: Array<Subscription> = []
+  constructor(protected _sandbox: UserSandbox) {
+    this.registerEvents();
+    this._sandbox.fetchProfile(this.token);
+  }
+
+  registerEvents() {
+    this.subscriptions.push(
+      this._sandbox.token$.subscribe((token) => this.token = token)
+    );
+    this.subscriptions.push(
+      this._sandbox.profile$.subscribe((user) => this.user = user)
+    )
+  }
+  ngOnInit(): void {
+  }
+
+}
