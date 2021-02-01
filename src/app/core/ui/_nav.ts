162ee5297d0533a485f0e9c492636d6724925421
Osama Khan <Osama6756@gmail.com>
2021-02-01 15:21:40 +0500

    Update _nav.ts

--- src/app/core/ui/_nav.ts
+++ src/app/core/ui/_nav.ts
@@ -12,56 +12,15 @@ export const navItems: INavData[] = [
     name: 'User',
     url: '/users',
     icon: 'icon-user',
-    children: [
-      {
-        name: 'List',
-        url: '/users',
-        icon: 'icon-list',
-      },
-      {
-        name: 'Add',
-        url: '/users/create',
-        icon: 'icon-plus',
-      },
-    ],
   },
   {
     name: 'Roles',
     url: '/authorization/roles',
     icon: 'icon-people',
-    children: [
-      {
-        name: 'List',
-        url: '/authorization/roles/list',
-        icon: 'icon-list',
-      },
-      {
-        name: 'Create',
-        url: '/authorization/roles/create',
-        icon: 'icon-plus',
-      },
-      {
-        name: 'Manage',
-        url: '/authorization/roles/manage',
-        icon: 'icon-wrench',
-      },
-    ]
   },
   {
     name: 'Permissions',
     url: '/authorization/permissions',
     icon: 'icon-check',
-    children: [
-      {
-        name: 'List',
-        url: '/authorization/permissions/list',
-        icon: 'icon-list',
-      },
-      {
-        name: 'Assign',
-        url: '/authorization/permissions/assign',
-        icon: 'icon-link',
-      },
-    ]
   },
 ];
