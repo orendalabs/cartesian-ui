a512980ae0ad7744050638ce36220f3944c627ff
Osama Khan <Osama6756@gmail.com>
2021-02-03 18:11:03 +0500

    Fixed title of UserUpdateComponent

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
         component: UserUpdateComponent,
         pathMatch: "full",
         data: {
-          title: 'Create User',
+          title: 'Edit User',
         },
       },
       {
