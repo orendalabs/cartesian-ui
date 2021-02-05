7de3405b236bcda15b61eb113bd62054774bf260
Osama Khan <Osama6756@gmail.com>
2021-02-05 12:49:25 +0500

    Added checkbox handling functions and properties

--- src/app/core/ui/components/listing-controls.component.ts
+++ src/app/core/ui/components/listing-controls.component.ts
@@ -6,11 +6,15 @@ import { IPaginationModel } from '../../models';
 export abstract class ListingControlsComponent<
   TDataModel,
   TSearchFormModel
-> extends BaseComponent {
+  > extends BaseComponent {
   criteria: RequestCriteria<TSearchFormModel>;
   data: Array<TDataModel>;
   pagination: IPaginationModel;
   isTableLoading = false;
+
+  // Checkbox properties
+  selectedItemIds: Array<string> = [];
+  checkedBoxes = [];
 
   constructor(injector: Injector) {
     super(injector);
@@ -47,6 +51,33 @@ export abstract class ListingControlsComponent<
 
   covertOffsetToPageNumber(offset: number): number {
     return offset + 1;
+  }
+
+  // Checkbox handling functions
+  onListItemCheckBoxChange(event, id) {
+    if (event.target.checked) {
+      this.onCheckItem(event.target, id);
+      this.checkedBoxes.push(event.target);
+    } else {
+      this.onUncheckItem(event.target, id);
+    }
+  }
+
+  onCheckItem(checkBox: any, id: string) {
+    this.selectedItemIds.push(id);
+    this.checkedBoxes.push(checkBox);
+  }
+
+  onUncheckItem(checkBox: any, id: string) {
+    const itemIndex = this.selectedItemIds.indexOf(id);
+    const boxIndex = this.checkedBoxes.indexOf(checkBox);
+    this.selectedItemIds.splice(itemIndex, 1);
+    this.checkedBoxes.splice(boxIndex, 1);
+  }
+
+  resetCheckBoxes() {
+    this.selectedItemIds = [];
+    this.checkedBoxes.forEach((checkBox) => (checkBox.checked = false));
   }
 
   protected abstract list(): void;
