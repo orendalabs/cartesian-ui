4d3880f40b6d583eb036d75f96c322f8322844de
Osama Khan <Osama6756@gmail.com>
2021-02-25 02:16:47 +0500

    Added routing for LocationModule

--- src/app/location/location-routing.module.ts
+++ src/app/location/location-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LocationComponent } from './location.component';
+import { CityCreateComponent } from './ui/city/city-create/city-create.component';
 import { CityListComponent } from './ui/city/city-list/city-list.component';
+import { CountryCreateComponent } from './ui/country/country-create/country-create.component';
+import { CountryListComponent } from './ui/country/country-list/country-list.component';
+import { LocationCreateComponent } from './ui/location/location-create/location-create.component';
+import { LocationListComponent } from './ui/location/location-list/location-list.component';
+import { StateCreateComponent } from './ui/state/state-create/state-create.component';
+import { StateListComponent } from './ui/state/state-list/state-list.component';
 
 
 const routes: Routes = [
@@ -11,12 +18,78 @@ const routes: Routes = [
     data: {},
     children: [
       {
-        path: '',
-        component: CityListComponent,
+        path: 'list',
+        component: LocationListComponent,
         pathMatch: 'full',
+        data: {
+          title: 'Locations',
+        },
+      },
+      {
+        path: 'create',
+        component: LocationCreateComponent,
+        pathMatch: 'full',
+        data: {
+          title: 'Create Location',
+        },
+      },
+      {
+        path: 'cities',
         data: {
           title: 'Cities',
         },
+        children: [
+          {
+            path: '',
+            component: CityListComponent,
+            data: { title: 'Cities' },
+          },
+          {
+            path: 'create',
+            component: CityCreateComponent,
+            data: { title: 'Create City' },
+          },
+        ]
+      },
+      {
+        path: 'countries',
+        data: {
+          title: 'Countries',
+        },
+        children: [
+          {
+            path: '',
+            component: CountryListComponent,
+            data: { title: 'Countries' },
+          },
+          {
+            path: 'create',
+            component: CountryCreateComponent,
+            data: { title: 'Create Country' },
+          },
+        ]
+      },
+      {
+        path: 'states',
+        data: {
+          title: 'States',
+        },
+        children: [
+          {
+            path: '',
+            component: StateListComponent,
+            data: { title: 'States' },
+          },
+          {
+            path: 'create',
+            component: StateCreateComponent,
+            data: { title: 'Create State' },
+          },
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'list'
       },
     ],
   },
