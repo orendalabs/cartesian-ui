21c8902a1438991358f7e28f830b66df83c12471
Osama Khan <Osama6756@gmail.com>
2021-02-25 02:28:15 +0500

    Implemented CreateCityComponent

--- src/app/location/ui/city/city-create/city-create.component.ts
+++ src/app/location/ui/city/city-create/city-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LocationSandbox } from '@app/location/location.sandbox';
+import { CityCreateForm } from '@app/location/models/form';
+import { FormHelper } from '@app/shared/helpers';
 
 @Component({
   selector: 'city-create',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CityCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({
+    countryId: new FormControl('', Validators.required),
+    stateId: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    latitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-90), Validators.max(90)]),
+    longitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-180), Validators.max(180)]),
+  });
+  constructor(protected _sandbox: LocationSandbox) { }
 
   ngOnInit(): void {
     
+  }
+
+  create(): void {
+    if(this.formGroup.valid) {
+      const form = new CityCreateForm({
+        countryId: this.formGroup.controls['countryId'].value,
+        stateId: this.formGroup.controls['stateId'].value,
+        name: this.formGroup.controls['name'].value,
+        latitude: this.formGroup.controls['latitude'].value,
+        longitude: this.formGroup.controls['longitude'].value,
+      });
+      this._sandbox.createCity(form);
+    }
+  }
+
+  getFormClasses(controlName: string): string {
+    const control = this.formGroup.controls[controlName];
+    if (control.valid) {
+      return 'is-valid';
+    } else if (control.dirty && control.touched) {
+      return 'is-invalid';
+    }
   }
 
 }
