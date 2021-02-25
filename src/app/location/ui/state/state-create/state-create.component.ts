67f7b0d68ba8435b10359883ef11863c7e2f3d0b
Osama Khan <Osama6756@gmail.com>
2021-02-25 20:51:46 +0500

    Added StateCreateComponent

--- src/app/location/ui/state/state-create/state-create.component.ts
+++ src/app/location/ui/state/state-create/state-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LocationSandbox } from '@app/location/location.sandbox';
+import { StateCreateForm } from '@app/location/models/form';
 
 @Component({
   selector: 'state-create',
@@ -6,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StateCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup = new FormGroup({
+    countryId: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    code: new FormControl('', Validators.required),
+  });
+  constructor(protected _sandbox: LocationSandbox) { }
 
   ngOnInit(): void {
+    
+  }
+
+  create(): void {
+    if(this.formGroup.valid) {
+      const form = new StateCreateForm({
+        countryId: this.formGroup.controls['countryId'].value,
+        name: this.formGroup.controls['name'].value,
+        code: this.formGroup.controls['code'].value,
+      });
+      this._sandbox.createState(form);
+    }
+  }
+
+  getFormClasses(controlName: string): string {
+    const control = this.formGroup.controls[controlName];
+    if (control.valid) {
+      return 'is-valid';
+    } else if (control.dirty && control.touched) {
+      return 'is-invalid';
+    }
   }
 
 }
