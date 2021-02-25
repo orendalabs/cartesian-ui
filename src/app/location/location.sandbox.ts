da23c3a3eab791940478bebbb213983fd9c19af9
Osama Khan <Osama6756@gmail.com>
2021-02-25 06:05:26 +0500

    Added fetch and create for country, location and state

--- src/app/location/location.sandbox.ts
+++ src/app/location/location.sandbox.ts
@@ -21,8 +21,21 @@ export class LocationSandbox extends Sandbox {
 
   private subscriptions: Array<Subscription> = [];
   
+  // City Observables
   citiesData$ = this.store.pipe(select(locationSelectors.getCitiesList));
   citiesMeta$ = this.store.pipe(select(locationSelectors.getCitiesMeta));
+
+  // Country Observables
+  countriesData$ = this.store.pipe(select(locationSelectors.getCountriesList));
+  countriesMeta$ = this.store.pipe(select(locationSelectors.getCountriesMeta));
+
+  // Location Observables
+  locationsData$ = this.store.pipe(select(locationSelectors.getLocationsList));
+  locationsMeta$ = this.store.pipe(select(locationSelectors.getLocationsMeta));
+
+  // State Observables
+  statesData$ = this.store.pipe(select(locationSelectors.getStatesList));
+  statesMeta$ = this.store.pipe(select(locationSelectors.getStatesMeta));
 
   constructor(
     protected store: Store<LocationState>,
@@ -32,6 +45,7 @@ export class LocationSandbox extends Sandbox {
     super(injector);
   }
 
+  // City Methods
   fetchCities(criteria: RequestCriteria<SearchCityForm>): void {
     this.store.dispatch(
       locationActions.doFetchCities({ requestCriteria: criteria })
@@ -49,6 +63,65 @@ export class LocationSandbox extends Sandbox {
       locationActions.doCreateCity({ form: form })
     );
   }
+
+  // Country Methods
+  fetchCountries(criteria: RequestCriteria<SearchCountryForm>): void {
+    this.store.dispatch(
+      locationActions.doFetchCountries({ requestCriteria: criteria })
+    );
+  }
+
+  fetchCountry(id: string): void {
+    this.store.dispatch(
+      locationActions.doFetchCountry({ id: id })
+    );
+  }
+
+  createCountry(form: CountryCreateForm): void {
+    this.store.dispatch(
+      locationActions.doCreateCountry({ form: form })
+    );
+  }
+
+  // Location Methods
+  fetchLocations(criteria: RequestCriteria<SearchLocationForm>): void {
+    this.store.dispatch(
+      locationActions.doFetchLocations({ requestCriteria: criteria })
+    );
+  }
+
+  fetchLocation(id: string): void {
+    this.store.dispatch(
+      locationActions.doFetchLocation({ id: id })
+    );
+  }
+
+  createLocation(form: LocationCreateForm): void {
+    this.store.dispatch(
+      locationActions.doCreateLocation({ form: form })
+    );
+  }
+
+  // State Methods
+  fetchStates(criteria: RequestCriteria<SearchStateForm>): void {
+    this.store.dispatch(
+      locationActions.doFetchStates({ requestCriteria: criteria })
+    );
+  }
+
+  fetchState(id: string): void {
+    this.store.dispatch(
+      locationActions.doFetchState({ id: id })
+    );
+  }
+
+  createState(form: StateCreateForm): void {
+    this.store.dispatch(
+      locationActions.doCreateState({ form: form })
+    );
+  }
+
+
   /**
    * Unsubscribe from events
    */
