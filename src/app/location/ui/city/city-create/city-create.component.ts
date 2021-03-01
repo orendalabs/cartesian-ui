f102d61fad48726a619e64a515aec40b4b5d1ce6
Osama Khan <Osama6756@gmail.com>
2021-03-01 14:56:06 +0500

    Added dropdowns, Improved form classes
    - Dropdowns for country and state selections
    - Replaced mt-2 classes with form-group

--- src/app/location/ui/city/city-create/city-create.component.ts
+++ src/app/location/ui/city/city-create/city-create.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LocationSandbox } from '@app/location/location.sandbox';
-import { CityCreateForm } from '@app/location/models/form';
+import { City, Country } from '@app/location/models/domain';
+import { CityCreateForm, SearchCityForm, SearchCountryForm, SearchStateForm } from '@app/location/models/form';
 import { FormHelper } from '@app/shared/helpers';
+import { RequestCriteria } from '@cartesian-ui/ng-axis';
+import { State } from '@app/location/models/domain/state.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'city-create',
@@ -12,15 +16,25 @@ export class CityCreateComponent implements OnInit {
 
   formGroup = new FormGroup({
     countryId: new FormControl('', Validators.required),
-    stateId: new FormControl('', Validators.required),
+    stateId: new FormControl(''),
     name: new FormControl('', Validators.required),
     latitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-90), Validators.max(90)]),
     longitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-180), Validators.max(180)]),
   });
+
+  subscriptions: Subscription[] = [];
+
+  countries: Country[] = [];
+  countriesCriteria = new RequestCriteria<SearchCountryForm>(new SearchCountryForm()).limit(100000);
+
+  states: State[] = [];
+  statesCriteria = new RequestCriteria<SearchStateForm>(new SearchStateForm()).limit(100000);
+
   constructor(protected _sandbox: LocationSandbox) { }
 
   ngOnInit(): void {
-    
+    this.registerEvents();
+    this._sandbox.fetchCountries(this.countriesCriteria);
   }
 
   create(): void {
@@ -36,8 +50,57 @@ export class CityCreateComponent implements OnInit {
     }
   }
 
+  onCountryInputChange(event): void {
+    const id = event.target.value;
+    this.states = null;
+    this.formGroup.controls["stateId"].reset();
+    this.statesCriteria.where("country_id", "=", id);
+    this._sandbox.fetchStates(this.statesCriteria);
+  }
+
   getFormClasses(controlName: string): string {
     const control = this.formGroup.controls[controlName];
     return FormHelper.getFormClasses(control);
+  }
+
+  registerEvents(): void {
+    this.subscriptions.push(
+      this._sandbox.countriesData$.subscribe((c: Country[]) => { 
+        if (c) {
+          this.countries = Object.values(c);
+          this.setCountryValidators();
+        }
+      })
+    );
+    this.subscriptions.push(
+      this._sandbox.statesData$.subscribe((s: State[]) => {
+        if (s) {
+          this.states = Object.values(s);
+          this.setStateValidators();
+        }
+      })
+    );
+  }
+
+  unregisterEvents(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
+
+  setCountryValidators(): void {
+    const control = this.formGroup.controls["stateId"];
+    const countryIds = this.countries.map((c) => c.id.toString());
+    control.setValidators([Validators.required, FormHelper.inValidator(countryIds)]);
+    control.updateValueAndValidity();
+  }
+
+  setStateValidators(): void {
+    const control = this.formGroup.controls["stateId"];
+    if (this.states.length == 0) {
+      control.clearValidators();
+    } else {
+      const stateIds = this.states.map((s) => s.id.toString());
+      control.setValidators([Validators.required, FormHelper.inValidator(stateIds)]);
+    }
+    control.updateValueAndValidity();
   }
 }
--- src/app/location/ui/city/city-detail/city-detail.component.ts
+++ src/app/location/ui/city/city-detail/city-detail.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { LocationSandbox } from '@app/location/location.sandbox';
-import { City } from '@app/location/models/domain';
+import { City, Country } from '@app/location/models/domain';
+import { SearchCountryForm, SearchStateForm } from '@app/location/models/form';
 import { CityUpdateForm } from '@app/location/models/form/update-city.model';
 import { FormHelper } from '@app/shared/helpers';
+import { RequestCriteria } from '@cartesian-ui/ng-axis';
+import { State } from '@app/location/models/domain';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -15,23 +18,39 @@ export class CityDetailComponent implements OnInit {
 
   formGroup = new FormGroup({
     countryId: new FormControl('', Validators.required),
-    stateId: new FormControl('', Validators.required),
+    stateId: new FormControl(''),
     name: new FormControl('', Validators.required),
     latitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-90), Validators.max(90)]),
     longitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-180), Validators.max(180)]),
   });
 
   subscriptions: Array<Subscription> = [];
+  
   city: City;
   loaded: boolean;
   loading: boolean;
   failed: boolean;
+
+  countries: Country[] = [];
+  countriesCriteria = new RequestCriteria<SearchCountryForm>(new SearchCountryForm()).limit(100000);
+
+  states: State[] = [];
+  statesCriteria = new RequestCriteria<SearchStateForm>(new SearchStateForm()).limit(100000);
 
   constructor(protected _sandbox: LocationSandbox,
     protected route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.registerEvents();
+    this._sandbox.fetchCountries(this.countriesCriteria);
+  }
+
+  onCountryInputChange(event): void {
+    const id = event.target.value;
+    this.states = null;
+    this.formGroup.controls['stateId'].reset('');
+    this.statesCriteria.where('country_id', '=', id);
+    this._sandbox.fetchStates(this.statesCriteria);
   }
 
   save(): void {
@@ -44,12 +63,12 @@ export class CityDetailComponent implements OnInit {
         latitude: this.formGroup.controls['latitude'].value,
         longitude: this.formGroup.controls['longitude'].value
       });
-      this._sandbox.updateCity(form);
+      console.log(form);
     }
   }
 
   delete(): void {
-    if(confirm("Are you sure you want to delete city " + this.city.name + "?")) {
+    if(confirm('Are you sure you want to delete city ' + this.city.name + '?')) {
       this._sandbox.deleteCity(this.city.id);
     }
   }
@@ -73,10 +92,44 @@ export class CityDetailComponent implements OnInit {
     this.subscriptions.push(
       this._sandbox.city$.subscribe((city: City) => this.city = city)
     );
+    this.subscriptions.push(
+      this._sandbox.countriesData$.subscribe((c: Country[]) => { 
+        if (c) {
+          this.countries = Object.values(c);
+          this.setCountryValidators();
+        }
+      })
+    );
+    this.subscriptions.push(
+      this._sandbox.statesData$.subscribe((s: State[]) => {
+        if (s) {
+          this.states = Object.values(s);
+          this.setStateValidators();
+        }
+      })
+    );
   }
   
   getFormClasses(controlName: string): string {
     const control = this.formGroup.controls[controlName];
     return FormHelper.getFormClasses(control);
+  }
+
+  setCountryValidators(): void {
+    const control = this.formGroup.controls['stateId'];
+    const countryIds = this.countries.map((c) => c.id.toString());
+    control.setValidators([Validators.required, FormHelper.inValidator(countryIds)]);
+    control.updateValueAndValidity();
+  }
+
+  setStateValidators(): void {
+    const control = this.formGroup.controls['stateId'];
+    if (this.states.length == 0) {
+      control.clearValidators();
+    } else {
+      const stateIds = this.states.map((s) => s.id.toString());
+      control.setValidators([Validators.required, FormHelper.inValidator(stateIds)]);
+    }
+    control.updateValueAndValidity();
   }
 }
