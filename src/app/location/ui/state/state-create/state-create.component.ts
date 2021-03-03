da03802ee99144cabd3437c65bcc10bfdd3f5465
Osama Khan <Osama6756@gmail.com>
2021-03-03 07:13:15 +0500

    Updated module to use ConfigurableForm

--- src/app/location/ui/state/state-create/state-create.component.ts
+++ src/app/location/ui/state/state-create/state-create.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { LocationSandbox } from '@app/location/location.sandbox';
-import { Country, State } from '@app/location/models/domain';
-import { SearchCountryForm, SearchStateForm, StateCreateForm } from '@app/location/models/form';
+import { Country } from '@app/location/models/domain';
+import { SearchCountryForm, StateCreateForm } from '@app/location/models/form';
+import { FieldConfig } from '@app/shared/components/configurable-form/models/field-config.model';
 import { FormHelper } from '@app/shared/helpers';
 import { RequestCriteria } from '@cartesian-ui/ng-axis';
 import { Subscription } from 'rxjs';
@@ -13,17 +14,41 @@ import { Subscription } from 'rxjs';
 })
 export class StateCreateComponent implements OnInit {
 
-  formGroup = new FormGroup({
-    countryId: new FormControl('', Validators.required),
-    name: new FormControl('', Validators.required),
-    code: new FormControl('', Validators.required),
-  });
-
   subscriptions: Subscription[] = [];
 
-  countries: Country[] = [];
   countriesLoading: boolean;
   countriesCriteria = new RequestCriteria<SearchCountryForm>(new SearchCountryForm()).limit(100000);
+
+  config: FieldConfig[] = [
+    {
+      type: 'select',
+      label: 'Country',
+      name: 'countryId',
+      options: [],
+      placeholder: 'Select Country...',
+      validation: [Validators.required],
+    },
+    {
+      type: 'input',
+      label: 'Name',
+      name: 'name',
+      validation: [Validators.required],
+      placeholder: 'Enter name',
+    },
+    {
+      type: 'input',
+      label: 'Code',
+      name: 'code',
+      validation: [Validators.required],
+      placeholder: 'Enter code',
+    },
+    {
+      label: 'Create',
+      name: 'submit',
+      type: 'button',
+      classes: 'btn btn-primary pull-right',
+    },
+  ];
 
   constructor(protected _sandbox: LocationSandbox) { }
 
@@ -32,12 +57,12 @@ export class StateCreateComponent implements OnInit {
     this._sandbox.fetchCountries(this.countriesCriteria);
   }
 
-  create(): void {
-    if(this.formGroup.valid) {
+  create(group): void {
+    if (group.valid) {
       const form = new StateCreateForm({
-        countryId: this.formGroup.controls['countryId'].value,
-        name: this.formGroup.controls['name'].value,
-        code: this.formGroup.controls['code'].value,
+        countryId: group.controls['countryId'].value,
+        name: group.controls['name'].value,
+        code: group.controls['code'].value,
       });
       this._sandbox.createState(form);
     }
@@ -47,7 +72,13 @@ export class StateCreateComponent implements OnInit {
     this.subscriptions.push(
       this._sandbox.countriesData$.subscribe((c: Country[]) => { 
         if (c) {
-          this.countries = Object.values(c);
+          this.config[0].options = [];
+          Object.values(c).forEach((v) => {
+            this.config[0].options.push({
+              name: v.name,
+              value: v.id,
+            });
+          });
           this.setCountryValidators();
         }
       })
@@ -63,15 +94,10 @@ export class StateCreateComponent implements OnInit {
     this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
-  getFormClasses(controlName: string): string {
-    const control = this.formGroup.controls[controlName];
-    return FormHelper.getFormClasses(control);
-  }
-
   setCountryValidators(): void {
-    const control = this.formGroup.controls["countryId"];
-    const countryIds = this.countries.map((c) => c.id.toString());
-    control.setValidators([Validators.required, FormHelper.inValidator(countryIds)]);
-    control.updateValueAndValidity();
+    if (this.config[0].options) {
+      const countryIds = this.config[0].options.map((c) => c.value.toString());
+      this.config[0].validation = [Validators.required, FormHelper.inValidator(countryIds)];
+    }
   }
 }
