36b5f0abc056b9f3899dd6e9533e431707df8331
Osama Khan <Osama6756@gmail.com>
2021-03-04 08:05:57 +0500

    Implemented configurable forms

--- src/app/location/ui/location/location-create/location-create.component.ts
+++ src/app/location/ui/location/location-create/location-create.component.ts
@@ -7,53 +7,142 @@ import { RequestCriteria } from '@cartesian-ui/ng-axis';
 import { State } from '@app/location/models/domain';
 import { Subscription } from 'rxjs';
 import { LocationCreateForm, SearchCityForm, SearchCountryForm, SearchStateForm } from '../../../models/form/'
+import { FieldConfig } from '@app/shared/components/configurable-form/models/field-config.model';
+import { ISelectField } from '@app/shared/components/configurable-form/models/select-field.model';
+
+enum nameIndexMap {
+  "locatableType" = 0,
+  "locatableId" = 1,
+  "addressLine1" = 2,
+  "addressLine2" = 3,
+  "countryId" = 4,
+  "stateId" = 5,
+  "cityId" = 6,
+  "postCode" = 7,
+  "latitude" = 8,
+  "longitude" = 9,
+}
 
 @Component({
   selector: 'location-create',
   templateUrl: './location-create.component.html',
 })
+
 export class LocationCreateComponent implements OnInit {
 
   subscriptions: Subscription[] = [];
 
-  countries: Country[] = [];
   countriesLoading: boolean;
   countriesCriteria = new RequestCriteria<SearchCountryForm>(new SearchCountryForm()).limit(100000);
 
-  states: State[] = [];
   statesLoading: boolean;
   statesCriteria = new RequestCriteria<SearchStateForm>(new SearchStateForm()).limit(100000);
 
-  cities: City[] = [];
   citiesLoading: boolean;
   citiesCriteria = new RequestCriteria<SearchCityForm>(new SearchCityForm()).limit(100000);
 
-  formGroup = new FormGroup({
-    locatableType: new FormControl('', [Validators.required]),
-    locatableId: new FormControl('', [Validators.required]),
-    addressLine1: new FormControl('', [Validators.required]),
-    addressLine2: new FormControl('', [Validators.required]),
-    countryId: new FormControl('', [Validators.required]),
-    stateId: new FormControl(''),
-    cityId: new FormControl(''),
-    postCode: new FormControl('', [Validators.required]),
-    latitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-90), Validators.max(90)]),
-    longitude: new FormControl('', [Validators.required, FormHelper.isFloatValidator(), Validators.min(-180), Validators.max(180)]),
-  });
+  config: FieldConfig[];
+
   constructor(protected _sandbox: LocationSandbox) { }
 
   ngOnInit(): void {
+    this.initConfig();
     this.registerEvents();
     this._sandbox.fetchCountries(this.countriesCriteria);
   }
 
+  initConfig() {
+    this.config = [
+      {
+        type: 'input',
+        label: 'Locatable Type',
+        name: 'locatableType',
+        validation: [Validators.required],
+      },
+      {
+        type: 'input',
+        label: 'Locatable ID',
+        name: 'locatableId',
+        validation: [Validators.required],
+      },
+      {
+        type: 'input',
+        label: 'Address Line 1',
+        name: 'addressLine1',
+        validation: [Validators.required],
+      },
+      {
+        type: 'input',
+        label: 'Address Line 2',
+        name: 'addressLine2',
+        validation: [Validators.required],
+      },
+      {
+        type: 'select',
+        label: 'Country',
+        name: 'countryId',
+        options: [],
+        placeholder: 'Select Country...',
+        validation: [Validators.required],
+      },
+      {
+        type: 'select',
+        label: 'State',
+        name: 'stateId',
+        options: [],
+        hidden: true,
+        placeholder: 'Select State...',
+      },
+      {
+        type: 'select',
+        label: 'City',
+        name: 'cityId',
+        options: [],
+        hidden: true,
+        placeholder: 'Select City...',
+      },
+      {
+        type: 'input',
+        label: 'Post Code',
+        name: 'postCode',
+        validation: [Validators.required],
+      },
+      {
+        type: 'input',
+        label: 'Latitude',
+        name: 'latitude',
+        validation: [Validators.required, FormHelper.isFloatValidator(), Validators.min(-90), Validators.max(90)],
+      },
+      {
+        type: 'input',
+        label: 'Longitude',
+        name: 'longitude',
+        validation: [Validators.required, FormHelper.isFloatValidator(), Validators.min(-180), Validators.max(180)],
+      },
+      {
+        label: 'Create',
+        name: 'submit',
+        type: 'button',
+        classes: 'btn btn-primary pull-right',
+      },
+    ];
+    console.log(this.config);
+    this.config[nameIndexMap.countryId].change = this.onCountryInputChange;
+    this.config[nameIndexMap.stateId].change = this.onStateInputChange;
+  }
+
   onCountryInputChange(event): void {
     const id = event.target.value;
-    this.states = null;
-    this.formGroup.controls["stateId"].reset("");
     
-    this.cities = [];
-    this.formGroup.controls["cityId"].reset("");  
+    const stateControl = this.config[nameIndexMap.stateId];
+    stateControl.options = null;
+    stateControl.hidden = true;
+    stateControl.value = '';
+  
+    const cityControl = this.config[nameIndexMap.cityId];
+    cityControl.options = null;
+    cityControl.hidden = true;
+    cityControl.value = '';
     this.setCityValidators();
 
     this.statesCriteria.where("country_id", "=", id);
@@ -62,40 +151,46 @@ export class LocationCreateComponent implements OnInit {
 
   onStateInputChange(event): void {
     const id = event.target.value;
-    this.cities = null;
-    this.formGroup.controls["cityId"].reset("");
+
+    const cityControl = this.config[nameIndexMap.cityId];
+    cityControl.options = null;
+    cityControl.hidden = true;
+    cityControl.value = '';
+
     this.citiesCriteria.where("state_id", "=", id);
     this._sandbox.fetchCities(this.citiesCriteria);
   }
 
-  create(): void {
-    if (this.formGroup.valid) {
+  create(group): void {
+    if (group.valid) {
       const form = new LocationCreateForm({
-        locatableType: this.formGroup.controls['locatableType'].value,
-        locatableId: this.formGroup.controls['locatableId'].value,
-        addressLine1: this.formGroup.controls['addressLine1'].value,
-        addressLine2: this.formGroup.controls['addressLine2'].value,
-        countryId: this.formGroup.controls['countryId'].value,
-        stateId: this.formGroup.controls['stateId'].value,
-        cityId: this.formGroup.controls['cityId'].value,
-        postCode: this.formGroup.controls['postCode'].value,
-        latitude: this.formGroup.controls['latitude'].value,
-        longitude: this.formGroup.controls['longitude'].value,
+        locatableType: group.controls['locatableType'].value,
+        locatableId: group.controls['locatableId'].value,
+        addressLine1: group.controls['addressLine1'].value,
+        addressLine2: group.controls['addressLine2'].value,
+        countryId: group.controls['countryId'].value,
+        stateId: group.controls['stateId'].value,
+        cityId: group.controls['cityId'].value,
+        postCode: group.controls['postCode'].value,
+        latitude: group.controls['latitude'].value,
+        longitude: group.controls['longitude'].value,
       });
-      this._sandbox.createLocation(form);
+      console.log(form)
+      // this._sandbox.createLocation(form);
     }
-  }
-
-  getFormClasses(controlName: string): string {
-    const control = this.formGroup.controls[controlName];
-    return FormHelper.getFormClasses(control);
   }
 
   registerEvents(): void {
     this.subscriptions.push(
       this._sandbox.countriesData$.subscribe((c: Country[]) => {
         if (c) {
-          this.countries = Object.values(c);
+          this.config[nameIndexMap.countryId].options = Object.values(c)
+            .map((v): ISelectField => {
+            return {
+              name: v.name, 
+              value: v.id
+            }
+          });
           this.setCountryValidators();
         }
       })
@@ -107,8 +202,15 @@ export class LocationCreateComponent implements OnInit {
     );
     this.subscriptions.push(
       this._sandbox.statesData$.subscribe((s: State[]) => {
-        if (s) {
-          this.states = Object.values(s);
+        if (s && s.length > 0) {
+          this.config[nameIndexMap.stateId].options = Object.values(s)
+            .map((v): ISelectField => {
+            return {
+              name: v.name, 
+              value: v.id
+            }
+          });
+          this.config[nameIndexMap.stateId].hidden = false;
           this.setStateValidators();
         }
       })
@@ -120,8 +222,15 @@ export class LocationCreateComponent implements OnInit {
     );
     this.subscriptions.push(
       this._sandbox.citiesData$.subscribe((c: City[]) => {
-        if (c) {
-          this.cities = Object.values(c);
+        if (c && c.length > 0) {
+          this.config[nameIndexMap.cityId].options = Object.values(c)
+            .map((v): ISelectField => {
+            return {
+              name: v.name, 
+              value: v.id
+            }
+          });
+          this.config[nameIndexMap.cityId].hidden = false;
           this.setCityValidators();
         }
       })
@@ -138,31 +247,28 @@ export class LocationCreateComponent implements OnInit {
   }
 
   setCountryValidators(): void {
-    const control = this.formGroup.controls["countryId"];
-    const countryIds = this.countries.map((c) => c.id.toString());
-    control.setValidators([FormHelper.inValidator(countryIds)]);
-    control.updateValueAndValidity();
+    const control = this.config[nameIndexMap.countryId];
+    const countryIds = control.options.map((c) => c.value.toString());
+    control.validation = [FormHelper.inValidator(countryIds)];
   }
 
   setStateValidators(): void {
-    const control = this.formGroup.controls["stateId"];
-    if (this.states.length == 0) {
-      control.clearValidators();
+    const control = this.config[nameIndexMap.stateId];
+    if (control.options.length == 0) {
+      control.validation = [];
     } else {
-      const stateIds = this.states.map((s) => s.id.toString());
-      control.setValidators([FormHelper.inValidator(stateIds)]);
+      const stateIds = control.options.map((s) => s.value.toString());
+      control.validation = [FormHelper.inValidator(stateIds)];
     }
-    control.updateValueAndValidity();
   }
 
   setCityValidators(): void {
-    const control = this.formGroup.controls["cityId"];
-    if (this.cities.length == 0) {
-      control.clearValidators();
+    const control = this.config[nameIndexMap.cityId]
+    if (control.options.length == 0) {
+      control.validation = [];
     } else {
-      const cityIds = this.cities.map((c) => c.id.toString());
-      control.setValidators([FormHelper.inValidator(cityIds)]);
+      const cityIds = control.options.map((c) => c.value.toString());
+      control.validation = [FormHelper.inValidator(cityIds)];
     }
-    control.updateValueAndValidity();
   }
 }
