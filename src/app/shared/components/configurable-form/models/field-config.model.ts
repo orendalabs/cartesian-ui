325724f7f55c8f298e0d23d48abd6f54e2f04647
Osama Khan <Osama6756@gmail.com>
2021-03-04 08:03:36 +0500

    Added more properties for field configuration

--- src/app/shared/components/configurable-form/models/field-config.model.ts
+++ src/app/shared/components/configurable-form/models/field-config.model.ts
@@ -1,15 +1,18 @@
 import { ValidatorFn } from '@angular/forms';
+import { ISelectField } from './select-field.model';
 
 export interface FieldConfig {
   disabled?: boolean,
   label?: string,
   name: string,
   inputType?: string,
-  options?: any[],
+  hidden?: boolean,
+  options?: ISelectField[],
   placeholder?: string,
   type: string,
   validation?: ValidatorFn[],
   value?: any,
   classes?: string,
-  click?: any
+  click?: (event) => void,
+  change?: (event) => void
 }
